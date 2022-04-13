483473f4572d27a9d72404a5672445b4b340a4e0
“Vitor” <vhnpouza@gmail.com>
2022-04-13 00:31:40 -0300

    logo tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials'
-  ]
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
