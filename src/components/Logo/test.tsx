0fdd6bca1d8011976581522e3315afad67d2606d
“Vitor” <vhnpouza@gmail.com>
2022-04-13 00:48:20 -0300

    logo mobile and tests

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-
+import 'jest-styled-components'
 import Logo from '.'
 
 describe('<Logo />', () => {
@@ -28,5 +28,16 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     })
+  })
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
