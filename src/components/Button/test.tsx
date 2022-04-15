63c38134230ba53d3f39c875042b4dac60fa1d5e
vtrpza <vhnpouza@gmail.com>
2022-04-15 14:22:30 -0300

    polymorphism and css adjustments for button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -53,4 +53,16 @@ describe('<Button/>', () => {
     expect(screen.getByText(/buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
+  it('should render button as a link', () => {
+    renderWithTheme(
+      <Button as="a" href="/link" icon={<AddShoppingCart data-testid="icon" />}>
+        Buy now
+      </Button>
+    )
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
